5b4275bc9d6347c45c7a528f08dfd946948f8901
Alessia Bellisario <alessia@apollographql.com>
2022-11-15 17:00:25 -0500

    fix: add logging (#2)

---
+++ .changeset/green-dancers-poke.md
@@ -0,0 +1,7 @@
+---
+'good-snow-tire-demo': patch
+---
+
+# Do more logging
+
+Adding some more logging to debug a race condition 🏎
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('testing');
+    console.log('beep boop');
   }
   return a + b;
 };
