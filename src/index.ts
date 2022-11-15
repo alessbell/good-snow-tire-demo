54ada34b747cefe8b0d3108d60140ee366395c08
alessia <alessia@apollographql.com>
2022-11-15 16:59:09 -0500

    chore: fix main.yml

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('testing')
+    console.log('testing');
     console.log('beep boop');
   }
   return a + b;
