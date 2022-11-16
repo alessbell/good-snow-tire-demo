0b9e7fd64453ebc96cc109519e6c17f740050527
alessia <alessia@apollographql.com>
2022-11-16 13:59:40 -0500

    chore: fix scripts

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-# Original script: https://github.com/apollo-elements/apollo-elements/blob/main/scripts/prerelease.sh
 
-# TODO: change next to beta
 [ ! -f .changeset/pre.json ] \
-  && npx changeset pre enter next \
+  && npx changeset pre enter beta \
   || echo 'Already in pre mode'
 
-npx changeset version
+# run a versioning script which calls `npm i` to also update lockfile
+npm run ci:version
 git add .
 git commit -m "chore: version prerelease packages"
