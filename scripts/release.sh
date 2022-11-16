f8ada192b2b37c33bd2a19a20c248c4bcef2cbc1
alessia <alessia@apollographql.com>
2022-11-16 11:20:49 -0500

    chore: update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,14 @@
 
 if [ -f .changeset/pre.json ]; then
   npx changeset pre exit
+  npx changeset version
+  git add .
+  git commit -m "Exit prerelease mode and version packages"
+  npm run build
+  npx changeset publish
+  git push --follow-tags
 else
   echo "Not in prerelease mode"
+  npm run build
+  npx changeset publish
 fi
-
-npm run build
-npx changeset publish
