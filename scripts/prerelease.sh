2f3d6d0a355708fd761cefce3c199cd42696ae59
alessia <alessia@apollographql.com>
2022-11-15 20:56:10 -0500

    update workflows

---
+++ scripts/prerelease.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Original script: https://github.com/apollo-elements/apollo-elements/blob/main/scripts/prerelease.sh
+
+[ ! -f .changeset/pre.json ] \
+  && npx changeset pre enter next \
+  || echo 'Already in pre mode'
+
+npx changeset version
+git add .
+git commit -m "chore: version prerelease packages"
