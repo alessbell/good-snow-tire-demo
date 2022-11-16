1408e0b0ab2d6610df7e7649201b0ec3ddbced6e
Alessia Bellisario <alessia@apollographql.com>
2022-11-16 11:48:57 -0500

    chore!: drop support for addition (#19)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export const sum = (a: number, b: number) => {
-  return a + b;
-};
-
 export const subtract = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('here');
