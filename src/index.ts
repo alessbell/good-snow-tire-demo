a9d7411b1240a91a25565c2358989ebab1ffcd7f
Alessia Bellisario <alessia@apollographql.com>
2022-11-16 14:13:45 -0500

    feat: adds multiply function (#22)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export const subtract = (a: number, b: number) => {
   }
   return a - b;
 };
+
+export const multiply = (a: number, b: number) => {
+  return a * b;
+};
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,10 @@
-import { subtract } from '../src';
+import { subtract, multiply } from '../src';
 
 describe('blah', () => {
   it('also works', () => {
     expect(subtract(1, 1)).toEqual(0);
+  });
+  it('also works with multiplication', () => {
+    expect(multiply(2, 2)).toEqual(4);
   });
 });
