627a92f365a9a8ad6ac924d34a150faf5d19ae6b
alessia <alessia@apollographql.com>
2022-11-15 19:23:13 -0500

    feat: adds subtract fn

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export const sum = (a: number, b: number) => {
   }
   return a + b;
 };
+
+export const subtract = (a: number, b: number) => {
+  return a - b;
+};
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,10 @@
-import { sum } from '../src';
+import { sum, subtract } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+  it('also works', () => {
+    expect(subtract(1, 1)).toEqual(0);
   });
 });
