bcb30acd9cb112aab4b2410126c26b27080e7914
alessia <alessia@apollographql.com>
2022-11-16 14:09:57 -0500

    chore: fix prerelease version script

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -7,4 +7,4 @@
 # run a versioning script which calls `npm i` to also update lockfile
 npm run ci:version
 git add .
-git commit -m "chore: version prerelease packages"
+git commit -m "chore: version beta prerelease package"
