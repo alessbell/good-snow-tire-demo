22c69f69fe6f34888a41a83a7f6e65a006d7c03a
Alessia Bellisario <alessia@apollographql.com>
2022-11-16 11:48:57 -0500

    chore!: drop support for addition (#19)

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,9 +1,6 @@
-import { sum, subtract } from '../src';
+import { subtract } from '../src';
 
 describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
   it('also works', () => {
     expect(subtract(1, 1)).toEqual(0);
   });
