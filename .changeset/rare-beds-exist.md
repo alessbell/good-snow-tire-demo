58606d9c1333280edd7a81b50de4db578976ee2c
Alessia Bellisario <alessia@apollographql.com>
2022-11-15 19:25:38 -0500

    feat: adds subtract fn (#5)

---
+++ .changeset/rare-beds-exist.md
@@ -0,0 +1,7 @@
+---
+'good-snow-tire-demo': minor
+---
+
+Added subtract function
+
+This adds the ability to subtract (no pun intended)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+export const subtract = (a: number, b: number) => {
+  return a - b;
+};
