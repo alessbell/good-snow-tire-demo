377a44890156441b7a290ae0c6358ffcda88a7c5
alessia <alessia@apollographql.com>
2022-11-18 14:52:35 -0500

    feat: add returnFoo function

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,4 +1,4 @@
-import { subtract, multiply, divide } from '../src';
+import { subtract, multiply, divide, returnFoo } from '../src';
 
 describe('blah', () => {
   it('also works', () => {
@@ -9,5 +9,8 @@ describe('blah', () => {
   });
   it('also works with division', () => {
     expect(divide(4, 2)).toEqual(2);
+  });
+  it('also works with Foo', () => {
+    expect(returnFoo()).toEqual('foo');
   });
 });
