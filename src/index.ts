8b77f7da653944d8f7ffd02cabdc37a7fbb7d1a4
alessia <alessia@apollographql.com>
2022-11-22 16:13:41 -0500

    adds changeset

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,10 @@ export const multiply = (a: number, b: number) => {
   }
   return a * b;
 };
+
+export const add = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('multiplying');
+  }
+  return a + b;
+};
