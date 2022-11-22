03ee85f1100aff8e98fdebef33695bdc721a89ce
Alessia Bellisario <alessia@apollographql.com>
2022-11-22 16:15:57 -0500

    Feature 2.0 (#34)
    * feat!: breaking change, removes divide
    
    * adds changeset
    
    * adds changeset

---
+++ .changeset/spotty-plums-study.md
@@ -0,0 +1,5 @@
+---
+'good-snow-tire-demo': minor
+---
+
+Add function
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ export const multiply = (a: number, b: number) => {
   return a * b;
 };
 
-export const divide = (a: number, b: number) => {
-  return a / b;
+export const add = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('multiplying');
+  }
+  return a + b;
 };
