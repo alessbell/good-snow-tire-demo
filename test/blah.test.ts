77326a4bc5b3fbe4a4d58d211c001374c56dcad6
Alessia Bellisario <alessia@apollographql.com>
2022-12-07 08:35:02 -0500

    feat: add addition function (#40)

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,11 @@
-import { subtract, multiply, divide } from '../src';
+import { subtract, multiply, divide, add } from '../src';
 
 describe('blah', () => {
   it('also works', () => {
     expect(subtract(1, 1)).toEqual(0);
+  });
+  it('also works with additiohn', () => {
+    expect(add(1, 1)).toEqual(2);
   });
   it('also works with multiplication', () => {
     expect(multiply(2, 2)).toEqual(4);
