39a23649658d485b067435362bdedb54ee524b9d
alessia <alessia@apollographql.com>
2023-02-06 09:54:29 -0500

    remove add

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,3 @@ export const multiply = (a: number, b: number) => {
 export const divide = (a: number, b: number) => {
   return a / b;
 };
-
-export const add = (a: number, b: number) => {
-  return a + b;
-};
